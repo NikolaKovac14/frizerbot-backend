ce4f2a9cb51dbfd0cf050b4310d6d06ce9494467
Niko <nikokovac4@gmail.com>
2026-05-03 18:10:35 +0200

    izboljašn mobilni pogled hraminika in slug polje

--- server.js
+++ server.js
@@ -2187,7 +2187,7 @@ function buildAdminPage(salon) {
   .day-times input[type=time] {
     padding: 0;
     font-size: 13px;
-    width: 52px;  /* ← bilo je 44px */
+    width: 48px;
     border: none;
     background: transparent;
     color: var(--black);
